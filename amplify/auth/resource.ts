ea4887a1cd25420959913b930a8c5abc5f0ff3a2
Julia Kim <juliakimseohyeon@gmail.com>
2025-09-29 18:23:41 -0700

    Add external provider to auth resource file

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { secret, defineAuth } from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,32 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    phone: true,    
+    externalProviders:{
+              signInWithApple: {
+                clientId: secret('SIWA_CLIENT_ID'),
+                teamId: secret('SIWA_TEAM_ID'),
+                keyId: secret('SIWA_KEY_ID'),
+                privateKey: secret('SIWA_PRIVATE_KEY'),
+              },
+      loginWithAmazon: {
+        clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
+        clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET'),
+      },
+      facebook: {
+        clientId: secret('FACEBOOK_CLIENT_ID'),
+        clientSecret: secret('FACEBOOK_CLIENT_SECRET'),
+      },
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+      },
+      callbackUrls: [
+        'http://localhost:3000/profile',
+        'https://mywebsite.com/profile'
+      ],
+      logoutUrls: ['http://localhost:3000/', 'https://mywebsite.com'],
+
+    }
   },
 });
